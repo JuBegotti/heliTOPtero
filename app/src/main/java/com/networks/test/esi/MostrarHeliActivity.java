a14c403ce2615e89ca8b6976671fa994e6d25d56
ju <begottiju@gmail.com>
2017-10-30 20:46:31 -0200

    login, adicionar e mais umas coisinhas

--- app/src/main/java/com/networks/test/esi/MostrarHeliActivity.java
+++ app/src/main/java/com/networks/test/esi/MostrarHeliActivity.java
@@ -3,6 +3,7 @@ package com.networks.test.esi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MostrarHeliActivity extends AppCompatActivity {
@@ -11,7 +12,23 @@ public class MostrarHeliActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_heli);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle(R.string.perfil);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, BuscarInventarioActivity.class));
+                finishAffinity();
+                break;
+            default:break;
+        }
+        return true;
+    }
+
     public void irVerFotoHelinho(View view) {
         //startActivity(new Intent(this, PreferenciasActivity.class));
     }
